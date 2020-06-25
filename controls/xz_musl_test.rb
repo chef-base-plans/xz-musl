977c0bdccdca60b3ad8bd73fd8ab506b97022a17
Steven Marshall <SMarshall@chef.io>
2020-06-25 15:54:05 +0100

    [xz-musl] Various refactors of existing code
    Add inspec.lock to .gitignore
    Update README.md to the latest format
    Added control to test other binaries
    Updated inspec.yml to latest format
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/xz_musl_test.rb
+++ controls/xz_musl_test.rb
@@ -28,3 +28,46 @@ control 'core-plans-xz-musl' do
     its('stderr') { should be_empty }
   end
 end
+
+control 'core-plans-xz-musl-binaries' do
+  impact 1.0
+  title 'Ensure xz-musl works'
+  desc '
+  To test the xz-musl binaries we first find the package path.
+  Using the package path to locate the binaries, we check that they exist and are executable.
+  Then we run version checks on them to prove that they work.
+  '
+
+  hab_pkg_path = command("hab pkg path #{plan_ident}")
+  describe hab_pkg_path do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+  hab_pkg_path = hab_pkg_path.stdout.strip
+  version = hab_pkg_path.split('/')[5]
+
+  binaries_to_test = command("ls #{File.join(hab_pkg_path, 'bin')}")
+  describe binaries_to_test do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+  binaries_to_test = binaries_to_test.stdout.strip.split().to_a
+
+  binaries_to_test.each do |binary|
+    describe file("#{File.join(hab_pkg_path, 'bin', binary)}") do
+      it { should exist }
+      it { should be_executable }
+    end
+  end
+
+  # Introducing specificity introduces errors.
+  binaries_to_test.each do |binary|
+    describe command("#{File.join(hab_pkg_path, 'bin', binary)} --version") do
+      its('stdout') { should match /#{version}/ }
+      its('stderr') { should be_empty }
+      its('exit_status') { should eq 0 }
+    end
+  end
+end
